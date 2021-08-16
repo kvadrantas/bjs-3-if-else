21fae90bd8f10452e942ec14c808c6ede9610b86
Rolandas Seputis <vi.kvadrantas@gmail.com>
2021-08-16 09:21:54 +0300

    If else

---
+++ 3-if-else.js
@@ -0,0 +1,25 @@
+// IF ELSE
+
+var sk1 = 55;
+var sk2 = 15;
+if (sk1 <= sk2) {
+    console.log(sk1);
+    console.log(` mazesnis uz `);
+    console.log(sk2);
+} else {
+    console.log(sk1);
+    console.log(` didesnis uz `);
+    console.log(sk2);
+}
+
+//---------------------
+
+let sk3 = 0;
+
+if (sk3 > 0) {
+    console.log(`teigiamas`);
+} else if (sk3 === 0) {
+    console.log(`nulis`);
+} else {
+    console.log(`neigiamas`);
+}
